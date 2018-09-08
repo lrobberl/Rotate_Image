af3eb5534f167dd8679f42e39ed5d25c446c8a2b
Ema <emanuele.alberti@yahoo.it>
2018-09-08 20:09:06 +0200

    Final release

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
+"""
+We start from the outermost square, swapping in a cyclic motion all the elements
+When proceeding inward, the squares get smaller and smaller
+"""
+
 def cyclicSwap(m, init_i, init_j):
     matrixDim = len(m)
     limit_y = matrixDim - 1 - init_j
@@ -15,22 +20,7 @@ def cyclicSwap(m, init_i, init_j):
 
         offset += 1
 
-    # in case of an odd dimension, we have to swap one more time
-    # if (matrixDim - 2*init_i) % 2 == 1:
-    #     m[init_i][init_j+offset], m[init_i+offset][limit_y] = m[init_i+offset][limit_y], m[init_i][init_j+offset]
-    #     m[init_i][init_j+offset], m[limit_x][limit_y-offset] = m[limit_x][limit_y-offset], m[init_i][init_j+offset]
-    #     m[init_i][init_j+offset], m[limit_x-offset][init_j] = m[limit_x-offset][init_j], m[init_i][init_j+offset]
-
     return m
-
-
-
-"""
-
-We start from the outermost square, swapping in a cyclic motion all the elements
-When proceeding inward, the squares get smaller and smaller (nCols decrease by 2)
-
-"""
 
 matrix = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
 
